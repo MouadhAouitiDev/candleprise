0a8c274a8e76a788c52bdd00f675839582b5eace
Mouadh Aouiti <a.mouadh@emerya.fr>
2023-10-31 11:14:31 +0100

    env de dev

---
+++ wp-content/themes/candleprise/controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Home page 
+ * Description: A Page Template with a darker design.
+ */
+
+// Code to display Page goes here...
+
+$context = Timber::context();
+
+ 
+$context['HomeData'] = get_posts(); 
+
+
+
+
+
+
+Timber::render( '/pages/Home/index.twig', $context );
